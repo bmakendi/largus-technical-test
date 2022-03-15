9de6a7105ac757a310ab9bf9177ea77658c774b8
Bryan Makendi <bryan.makendi@gmail.com>
2022-03-15 01:14:25 +0100

    Setting up project, added sass, beginning with hooks and header, will add layout component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import styles from './header.module.scss'
+import defaultPicture from '../../assets/profile.png'
+
+const Header = () => {
+  return (
+    <header className={styles.container}>
+      <Image src={defaultPicture} alt='Photo de profil' height={40} width={40} />
+    </header>
+  )
+}
+
+export default Header
---
+++ src/utils/hooks/user.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { User } from "../../types/user";
+
+export const useGetUsers = (url: string) => {
+  const [users, setUsers] = useState<User[]>([])
+  const [isLoading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url)
+      const data = await response.json()
+      setUsers(data)
+      }
+      catch {
+        setError(true)
+      }
+    }
+    fetchData()
+    setLoading(false)
+  }, [url])
+
+  return { users, isLoading, error }
+}
+
+export const useGetUser = (url: string) => {
+  const [user, setUser] = useState<User>()
+  const [isLoading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url)
+        const data = await response.json()
+        setUser(data)
+      }
+      catch (error) {
+        console.log(error);
+        setError(true)
+      }
+    }
+    fetchData()
+    setLoading(false)
+  }, [url])
+
+  return { user, isLoading, error }
+}
